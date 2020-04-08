bcabebefc77ab3e1228db913f56c0938734d98c1
anoorzi <anoorzi123@gmail.com>
2020-04-08 04:56:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form')
+  let ul = document.querySelector('#tasks')
+
+  ul.addEventListener('click',function(event){
+    if(event.target.className==='del'){
+      let node = event.target
+      node.parentNode.remove()
+
+    }
+  })
+  form.addEventListener('submit',function(event){
+    event.preventDefault()
+    // let input = document.querySelector('#new-task-description')
+    //console.log(form.des.value)
+    let newPostList = document.getElementById('tasks')
+    let li = document.createElement('li')
+    li.innerHTML = `<p> ${form.des.value} </p>
+    <button class='del'>delete</button>`
+    newPostList.appendChild(li)
+    form.reset()
+  })
+
+
+
 });
